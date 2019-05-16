d483072f5d5ff32f6e3d188c417017e7250d2f59
Ruihao Huang <huangruihaocst@126.com>
2019-05-16 20:52:23 +0800

    [modify] 399.

--- 399.EvaluateDivision.py
+++ 399.EvaluateDivision.py
@@ -1,3 +1,4 @@
+# Union Search
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         n = len(equations)
@@ -43,3 +44,54 @@ class Solution:
         
         return res
 
+
+# DFS
+# class Solution:
+#     def __init__(self):
+#         self.graph = dict()
+
+#     def add(self, v1, v2, val):
+#         if v1 in self.graph:
+#             self.graph[v1][v2] = val
+#         else:
+#             self.graph[v1] = {v2: val}
+
+#     def query(self, v1, v2):
+#         ans = None
+#         current = 1
+#         visited = {v1}
+
+#         def visit(v):
+#             nonlocal ans, current, visited
+#             if v not in self.graph:
+#                 return
+#             if v == v2:
+#                 ans = current
+#                 return
+#             for c in self.graph[v]:
+#                 if c not in visited:
+#                     visited.add(c)
+#                     current *= self.graph[v][c]
+#                     visit(c)
+#                     visited.remove(c)
+#                     current /= self.graph[v][c]
+
+#         visit(v1)
+#         return ans if ans is not None else -1
+
+#     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+#         n = len(equations)
+
+#         # build graph
+#         for i in range(n):
+#             v1, v2 = equations[i]
+#             val = values[i]
+#             self.add(v1, v2, val)
+#             self.add(v2, v1, 1 / val)
+
+#         # query
+#         res = list()
+#         for v1, v2 in queries:
+#             res.append(self.query(v1, v2))
+
+#         return res
