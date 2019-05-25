d430b49e8d91348e5f208c1a095f8d8a9858b46c
Ruihao Huang <huangruihaocst@126.com>
2019-05-25 15:47:02 -0700

    [comment] 753.

--- 753.CrackingTheSafe.py
+++ 753.CrackingTheSafe.py
@@ -9,6 +9,9 @@ class Solution:
         def dfs(v):
             nonlocal visited, current
             
+            # important DFS trick here.
+            # use return True to skip further search,
+            # thus save time
             if len(visited) == target:
                 return True
             
@@ -17,6 +20,8 @@ class Solution:
                 if new not in visited:
                     visited.add(new)
                     current.append(d)
+                    # also here: if answer has already been found,
+                    # no need for further search
                     if dfs(new):
                         return True
                     visited.remove(new)
