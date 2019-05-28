0f1ae0bb400962ec7a7968ddd116bebb6f3f3614
Ruihao Huang <huangruihaocst@126.com>
2019-05-27 22:53:14 -0700

    [add] 1051.

---
+++ 1051.HeightChecker.py
@@ -0,0 +1,5 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        sorted_heights = sorted(heights)
+        return sum(1 for i in range(len(heights)) if heights[i] != sorted_heights[i])
+            
