88168495d1b09df7d3efd521e64cb54b0391d7f5
Ruihao Huang <huangruihaocst@126.com>
2019-08-27 19:48:41 -0700

    [add] 31.

---
+++ 31.NextPermutation.py
@@ -0,0 +1,19 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        if n < 2:
+            return
+        i = n - 2
+        while nums[i] >= nums[i + 1] and i >= 0:
+            i -= 1
+        if i == -1:
+            nums.reverse()
+            return
+        j = n - 1
+        while nums[j] <= nums[i]:
+            j -= 1
+        nums[i], nums[j] = nums[j], nums[i]
+        nums[i + 1:] = reversed(nums[i + 1:])
